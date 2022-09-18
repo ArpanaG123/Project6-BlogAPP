3a41f224c64047a1ae6e6595e6e45ba44337ee71
Arpana Kumari <kashyaparpana203@gmail.com>
2022-09-18 20:12:19 +0530

    change

--- blogapp/src/Components/FullData.jsx
+++ blogapp/src/Components/FullData.jsx
@@ -3,22 +3,24 @@ import { useParams } from "react-router-dom";
 import { store } from "./Data";
 import './Style.css'
 
+var _id;
 const Fulldata = () => {
   const [data] = useContext(store);
   const {id} = useParams();
+  _id = id - 1;
 
   return (
     <div>
       {
         <div className="leftData">
           <hr className="horizontal"></hr>
-          <div key = {data[id-1].id}>
-            <img src = {data[id-1].image} alt = '' className='imageUrlNext' />
-            <div><h3 className='titleNext'>{data[id-1].Title}</h3></div>
-            <div><p className='paraNext'>{data[id-1].Description}</p></div>
-            <div><p className='dateNext'>{data[id-1].Date}</p></div>
-            <div><p className="timingNext">{data[id-1].Time}</p></div>
-            <div><p className='catNext'>{data[id-1].Category}</p></div>
+          <div key = {data[_id].id}>
+            <img src = {data[_id].image} alt = '' className='imageUrlNext' />
+            <div><h3 className='titleNext'>{data[_id].Title}</h3></div>
+            <div><p className='paraNext'>{data[_id].Description}</p></div>
+            <div><p className='dateNext'>{data[_id].Date}</p></div>
+            <div><p className="timingNext">{data[_id].Time}</p></div>
+            <div><p className='catNext'>{data[_id].Category}</p></div>
           </div>
         </div>
       }
